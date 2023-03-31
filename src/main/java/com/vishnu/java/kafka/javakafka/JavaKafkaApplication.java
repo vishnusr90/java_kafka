ba2a3cc3ea9b0822b79262349346317491c20836
Vishnu <vishnusr01@gmail.com>
2023-03-31 22:39:20 +0800

    Initial commit

---
+++ src/main/java/com/vishnu/java/kafka/javakafka/JavaKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu.java.kafka.javakafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishnu/java/kafka/javakafka/JavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.java.kafka.javakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
