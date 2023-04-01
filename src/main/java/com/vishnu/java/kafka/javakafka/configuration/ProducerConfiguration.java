537c732efa67ab6d121a89a555c4b2a02f58bcd0
Vishnu <vishnusr01@gmail.com>
2023-04-01 19:09:25 +0800

    Fix issue - Add json serializer instead of string serializer

--- src/main/java/com/vishnu/java/kafka/javakafka/configuration/ProducerConfiguration.java
+++ src/main/java/com/vishnu/java/kafka/javakafka/configuration/ProducerConfiguration.java
@@ -10,11 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class ProducerConfiguration {
     
-    private static final String KAFKA_BROKER = "local:8081";
+    private static final String KAFKA_BROKER = "localhost:9092";
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
@@ -26,7 +27,7 @@ public class ProducerConfiguration {
         Map<String, Object> configurations = new HashMap<>();
         configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER);
         configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return configurations;
     }
 
