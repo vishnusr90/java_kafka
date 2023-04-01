bd91b90c1270e3f229e5ae01219e0d11eef68a04
Vishnu <vishnusr01@gmail.com>
2023-04-01 19:11:01 +0800

    Refactor controller class

--- src/main/java/com/vishnu/java/kafka/javakafka/KafkaController.java
+++ src/main/java/com/vishnu/java/kafka/javakafka/KafkaController.java
@@ -3,22 +3,27 @@ package com.vishnu.java.kafka.javakafka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kafka")
 public class KafkaController {
 
     @Autowired
     private KafkaTemplate<String, String> template;
 
-    @GetMapping()
+    @GetMapping("/health")
     public String health() {
         return "Up and running";
     }
 
-    @GetMapping
-    public void produce(String message) {
+    @GetMapping("/produce/{message}")
+    public void produce(@PathVariable String message) {
+        System.out.println("Producing message ...");
         this.template.send("my-topic", message);
+        System.out.println("Message sent ...");
     }
     
 }
